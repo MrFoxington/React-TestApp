54647b22f512144289171ef03f6691d17d5ca345
JohnFox <John@Fox>
2025-06-08 12:17:06 +1000

    Wrapped Main with ErrorBoundry for better debugging

--- src/main.jsx
+++ src/main.jsx
@@ -9,18 +9,21 @@ import ExpenseTracker from "./pages/ExpenseTracker";
 import TaskTrackerPage from "./pages/TaskTracker.jsx";
 import LocalStorageTestPage from "./pages/LocalStorageTest.jsx";
 import MyAppBar from "./MyAppBar";
+import ErrorBoundary from "./components/ErrorBoundry.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <MyAppBar />
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/about" element={<AboutUs />} />
-        <Route path="/expenseTracker" element={<ExpenseTracker />} />
-        <Route path="/taskTracker" element={<TaskTrackerPage />} />
-        <Route path="/localStoreTesting" element={<LocalStorageTestPage />} />
-      </Routes>
-    </BrowserRouter>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <MyAppBar />
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/expenseTracker" element={<ExpenseTracker />} />
+          <Route path="/taskTracker" element={<TaskTrackerPage />} />
+          <Route path="/localStoreTesting" element={<LocalStorageTestPage />} />
+        </Routes>
+      </BrowserRouter>
+    </ErrorBoundary>
   </StrictMode>
 );
